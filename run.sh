daf7035da72e1161d61d59ff97393e8cd335bfa7
mikiZhu <mcspero123@gmail.com>
2021-05-16 09:31:20 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repository_ssh_url=git@github.com:mikaizhu/Notes.git
+repository_ssh_url=git@github.com:mikaizhu/Speech-signal-processing.git
 git remote set-url origin $repository_ssh_url
 git add .
 echo "input commit reason..."
